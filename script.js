a9a63727ca032d317391dc8dae9dd53bca85ec49
bfly2001 <brendan.sflynn@gmail.com>
2020-08-14 15:43:22 -0700

    Update script.js
    Created array 'data' for users.

--- script.js
+++ script.js
@@ -4,3 +4,5 @@ const doubleBtn = document.getElementById('double');
 const showMillionairesBtn = document.getElementById('show-millionaires');
 const sortBtn = document.getElementById('sort');
 const calculateWealthBtn = document.getElementById('calculate-wealth');
+
+let data = [];

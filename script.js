e2bf5e48bd705528893c12943358748572628aa7
bfly2001 <brendan.sflynn@gmail.com>
2020-08-15 12:51:47 -0700

    Update script.js
    Added constant 'user' inside getRandomUser() function.

--- script.js
+++ script.js
@@ -16,5 +16,5 @@ async function getRandomUser() {
     const res = await fetch('https://randomuser.me/api');
     const data = await res.json();
 
-    console.log(data);
+    const user = data.results[0];
 }

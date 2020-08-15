c6b0ccfddad2b7c2ecc96f6d3e5014f1d301bef1
bfly2001 <brendan.sflynn@gmail.com>
2020-08-15 12:43:04 -0700

    Update script.js
    Created async function getRandomUser() using fetch and random user api.

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ const sortBtn = document.getElementById('sort');
 const calculateWealthBtn = document.getElementById('calculate-wealth');
 
 let data = [];
+
+//fetch random user and money
+async function getRandomUser() {
+    const res = await fetch('https://randomuser.me/api');
+    const data = await res.json();
+}

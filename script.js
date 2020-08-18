a62742452c2a61a5c10a178eb49bbe3e7de6b82d
bfly2001 <brendan.sflynn@gmail.com>
2020-08-17 20:28:32 -0700

    Update script.js
    Created show millionaires function.

--- script.js
+++ script.js
@@ -42,6 +42,13 @@ function sortByRichest() {
     updateDOM();
 }
 
+// Filter only millionaires
+function showMillionaires() {
+    data = data.filter(user => user.money > 1000000);
+
+    updateDOM();
+}
+
 // Add new obj to data arr
 function addData(obj) {
     data.push(obj);
